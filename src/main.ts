95362510814b48d53a25445a5d1634cc595269ef
Thomas HERVE <therve@descours-cabaud.net>
2024-04-06 09:48:05 +0200

    Add cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { WsAdapter } from '@nestjs/platform-ws';
 async function bootstrap() {
   const app = await NestFactory.create(FunnelModule);
   app.useWebSocketAdapter(new WsAdapter(app));
+  app.enableCors();
   await app.listen(8000);
 }
 bootstrap();
